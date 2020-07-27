4b2180a3f6c073bef625ae3aff216019c28cd98a
AdriFC <adri86vigo@gmail.com>
2020-07-27 17:19:03 +0200

    Creación de los primeros componentes de Angular

--- AprendiendoAngular/src/app/app.module.ts
+++ AprendiendoAngular/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { MiComponente } from './components/mi-componente/mi-componente.component';
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
 import { PruebasComponent } from './components/pruebas/pruebas.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -12,7 +16,11 @@ import { PruebasComponent } from './components/pruebas/pruebas.component';
     AppComponent,
     MiComponente,
     PeliculasComponent,
-    PruebasComponent
+    PruebasComponent,
+    HeaderComponent,
+    SliderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
